8ed84f033df472868e5d09becef86d4879590c45
vmqxdv <vquintilio@gmail.com>
2025-04-03 22:35:21 +0200

    feat: Importato stile

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,6 +3,7 @@ import Info from "./info"
 import Socials from "./socials"
 import './css/navigation.css'
 import './css/info.css'
+import './css/socials.css'
 
 export default function footer() {
   return (
