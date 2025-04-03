1d22d452e0a2df04eee5649213e80455db03643c
vmqxdv <vquintilio@gmail.com>
2025-04-03 21:54:00 +0200

    feat: Aggiunto stile footer navigation

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,7 @@
 import Navigation from "./navigation"
 import Info from "./info"
 import Socials from "./socials"
+import './style.css'
 
 export default function footer() {
   return (
--- src/components/footer/navigation.jsx
+++ src/components/footer/navigation.jsx
@@ -1,31 +1,31 @@
 export default function CTABar() {
   return (
-    <div>
+    <div class="navigation">
 
       <ul>
         <li>
           <a href="#">
-            <img src="src\assets\img\buy-comics-digital-comics.png" alt="#" /> DIGITAL COMICS
+            <img src="src\assets\img\buy-comics-digital-comics.png" alt="#" />DIGITAL COMICS
           </a>
         </li>
         <li>
           <a href="#">
-            <img src="src\assets\img\buy-comics-merchandise.png" alt="#" /> DC MERCHANDISE
+            <img src="src\assets\img\buy-comics-merchandise.png" alt="#" />DC MERCHANDISE
           </a>
         </li>
         <li>
           <a href="#">
-            <img src="src\assets\img\buy-comics-subscriptions.png" alt="#" /> SUBSCRIPTION
+            <img src="src\assets\img\buy-comics-subscriptions.png" alt="#" />SUBSCRIPTION
           </a>
         </li>
         <li>
           <a href="#">
-            <img src="src\assets\img\buy-comics-shop-locator.png" alt="#" /> COMIC SHOP LOCATOR
+            <img src="src\assets\img\buy-comics-shop-locator.png" alt="#" />COMIC SHOP LOCATOR
           </a>
         </li>
         <li>
           <a href="#">
-            <img src="src\assets\img\buy-dc-power-visa.svg" alt="#" /> DC POWER VISA
+            <img src="src\assets\img\buy-dc-power-visa.svg" alt="#" />DC POWER VISA
           </a>
         </li>
       </ul>
