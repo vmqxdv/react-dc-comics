e8ef66740185f088f2ab19df53b2895bfa502abd
vmqxdv <vquintilio@gmail.com>
2025-04-03 20:08:03 +0200

    feat: Dichiarato socials

---
+++ src/components/footer/socials.jsx
@@ -0,0 +1,30 @@
+export default function socials() {
+  return (
+    <div>
+
+      <button>SING-UP NOW</button>
+
+      <div>
+        <h6>FOLLOW US</h6>
+        <ul>
+          <li>
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-brands fa-pinterest"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-solid fa-location-dot"></i></a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  )
+};
