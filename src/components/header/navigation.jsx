fe2acce50c9b912e98f808864478a4abeb6043da
vmqxdv <vquintilio@gmail.com>
2025-04-03 19:39:40 +0200

    fix: Corretta posizione div

--- src/components/header/navigation.jsx
+++ src/components/header/navigation.jsx
@@ -7,44 +7,62 @@ export default function navigator() {
           <a href="#">CHARACTERS</a>
           <div class="selected-page-indicator"></div>
         </div>
+      </li>
+      <li>
         <div>
           <a href="#">COMICS</a>
           <div class="selected-page-indicator"></div>
         </div>
+      </li>
+      <li>
         <div>
           <a href="#">MOVIES</a>
           <div class="selected-page-indicator"></div>
         </div>
+      </li>
+      <li>
         <div>
           <a href="#">TV</a>
           <div class="selected-page-indicator"></div>
         </div>
+      </li>
+      <li>
         <div>
           <a href="#">GAMES</a>
           <div class="selected-page-indicator"></div>
         </div>
+      </li>
+      <li>
         <div>
           <a href="#">COLLECTIBLES</a>
           <div class="selected-page-indicator"></div>
         </div>
+      </li>
+      <li>
         <div>
           <a href="#">VIDEOS</a>
           <div class="selected-page-indicator"></div>
         </div>
+      </li>
+      <li>
         <div>
           <a href="#">FANS</a>
           <div class="selected-page-indicator"></div>
         </div>
+      </li>
+      <li>
         <div>
           <a href="#">NEWS</a>
           <div class="selected-page-indicator"></div>
         </div>
+      </li>
+      <li>
         <div>
           <a href="#">SHOP</a>
           <div class="selected-page-indicator"></div>
         </div>
       </li>
-
+      
     </ul>
   )
 };
