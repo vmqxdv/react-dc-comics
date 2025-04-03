73ac0221f912fe7839da19cf58de56b6b914e51c
vmqxdv <vquintilio@gmail.com>
2025-04-03 22:11:18 +0200

    feat: Aggiunto classi

--- src/components/footer/info.jsx
+++ src/components/footer/info.jsx
@@ -1,55 +1,61 @@
 export default function info() {
   return (
-    <div>
+    <div class="info">
 
-      <div>
-        <h6>DC COMICS</h6>
-        <ul>
-        <li><a href="#">Characters</a></li>
-        <li><a href="#">Comics</a></li>
-        <li><a href="#">Movies</a></li>
-        <li><a href="#">TV</a></li>
-        <li><a href="#">Games</a></li>
-        <li><a href="#">Videos</a></li>
-        <li><a href="#">News</a></li>
-      </ul>
+      <div className="links">
+        <div>
+          <h6>DC COMICS</h6>
+          <ul>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
 
+        </div>
+
+        <div>
+          <h6>DC</h6>
+          <ul>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy Policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+
+        <div>
+          <h6>SITES</h6>
+          <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+        </div>
+
+        <div>
+          <h6>SHOP</h6>
+          <ul>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
       </div>
 
-      <div>
-        <h6>DC</h6>
-        <ul>
-          <li><a href="#">Terms Of Use</a></li>
-          <li><a href="#">Privacy Policy (New)</a></li>
-          <li><a href="#">Ad Choices</a></li>
-          <li><a href="#">Advertising</a></li>
-          <li><a href="#">Jobs</a></li>
-          <li><a href="#">Subscriptions</a></li>
-          <li><a href="#">Talent Workshops</a></li>
-          <li><a href="#">CPSC Certificates</a></li>
-          <li><a href="#">Ratings</a></li>
-          <li><a href="#">Shop Help</a></li>
-          <li><a href="#">Contact Us</a></li>
-        </ul>
-      </div>
-
-      <div>
-        <h6>SITES</h6>
-        <ul>
-          <li><a href="#">DC</a></li>
-          <li><a href="#">MAD Magazine</a></li>
-          <li><a href="#">DC Kids</a></li>
-          <li><a href="#">DC Universe</a></li>
-          <li><a href="#">DC Power Visa</a></li>
-        </ul>
-      </div>
-
-      <div>
-        <h6>SHOP</h6>
-        <ul>
-          <li><a href="#">Shop DC</a></li>
-          <li><a href="#">Shop DC Collectibles</a></li>
-        </ul>
+      <div className="big-logo">
+        <img src="src\assets\img\dc-logo-bg.png" alt="#" />
       </div>
 
     </div>
