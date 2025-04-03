7f69aa560c4cb8f4f0ade6d40d016dc360d87148
vmqxdv <vquintilio@gmail.com>
2025-04-03 22:11:07 +0200

    feat: Differenzati file di stile

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,8 @@
 import Navigation from "./navigation"
 import Info from "./info"
 import Socials from "./socials"
-import './style.css'
+import './css/navigation.css'
+import './css/info.css'
 
 export default function footer() {
   return (
