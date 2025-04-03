f09a3b26692649aef09ce0272c9d2969bc6985da
vmqxdv <vquintilio@gmail.com>
2025-04-03 20:38:53 +0200

    feat: Modificate classi di stile

--- src/components/footer/navigation.jsx
+++ src/components/footer/navigation.jsx
@@ -1,30 +1,30 @@
 export default function CTABar() {
   return (
-    <div class="bg-primary text-light">
+    <div class="bg-primary">
 
-      <ul>
+      <ul class="d-flex justify-content-between">
         <li>
-          <a href="#">
+          <a href="#" class="text-light">
             <img src="src\assets\img\buy-comics-digital-comics.png" alt="#" /> DIGITAL COMICS
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="#" class="text-light">
             <img src="src\assets\img\buy-comics-merchandise.png" alt="#" /> DC MERCHANDISE
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="#" class="text-light">
             <img src="src\assets\img\buy-comics-subscriptions.png" alt="#" /> SUBSCRIPTION
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="#" class="text-light">
             <img src="src\assets\img\buy-comics-shop-locator.png" alt="#" /> COMIC SHOP LOCATOR
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="#" class="text-light">
             <img src="src\assets\img\buy-dc-power-visa.svg" alt="#" /> DC POWER VISA
           </a>
         </li>
