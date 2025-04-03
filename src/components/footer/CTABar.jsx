2461e5157846881d851bece02c8b7f638fe6a974
vmqxdv <vquintilio@gmail.com>
2025-04-03 19:48:56 +0200

    feat: Dichiarara CTABar

---
+++ src/components/footer/CTABar.jsx
@@ -0,0 +1,35 @@
+export default function CTABar() {
+  return (
+    <div>
+
+      <ul>
+        <li>
+          <a href="#">
+            <img src="src\assets\img\buy-comics-digital-comics.png" alt="#" /> DIGITAL COMICS
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="src\assets\img\buy-comics-digital-comics.png" alt="#" /> DC MERCHANDISE
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="src\assets\img\buy-comics-digital-comics.png" alt="#" /> SUBSCRIPTION
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="src\assets\img\buy-comics-digital-comics.png" alt="#" /> COMIC SHOP LOCATOR
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="src\assets\img\buy-comics-digital-comics.png" alt="#" /> DC POWER VISA
+          </a>
+        </li>
+      </ul>
+
+    </div>
+  )
+};
