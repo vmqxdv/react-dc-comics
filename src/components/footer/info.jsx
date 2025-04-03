aa04657a73b29a5aa430fdb41732ff65bc296828
vmqxdv <vquintilio@gmail.com>
2025-04-03 21:04:26 +0200

    feat: Trasformato testo in link

--- src/components/footer/info.jsx
+++ src/components/footer/info.jsx
@@ -1,53 +1,54 @@
 export default function info() {
   return (
-    <div>
+    <div class="d-flex">
 
       <div>
         <h6>DC COMICS</h6>
         <ul>
-          <li>Characters</li>
-          <li>Comics</li>
-          <li>Movies</li>
-          <li>TV</li>
-          <li>Games</li>
-          <li>Videos</li>
-          <li>News</li>
-        </ul>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">News</a></li>
+      </ul>
+
       </div>
 
       <div>
         <h6>DC</h6>
         <ul>
-          <li>Terms Of Use</li>
-          <li>Privacy Policy (New)</li>
-          <li>Ad Choices</li>
-          <li>Advertising</li>
-          <li>Jobs</li>
-          <li>Subscriptions</li>
-          <li>Talent Workshops</li>
-          <li>CPSC Certificates</li>
-          <li>Ratings</li>
-          <li>Shop Help</li>
-          <li>Contact Us</li>
+          <li><a href="#">Terms Of Use</a></li>
+          <li><a href="#">Privacy Policy (New)</a></li>
+          <li><a href="#">Ad Choices</a></li>
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Subscriptions</a></li>
+          <li><a href="#">Talent Workshops</a></li>
+          <li><a href="#">CPSC Certificates</a></li>
+          <li><a href="#">Ratings</a></li>
+          <li><a href="#">Shop Help</a></li>
+          <li><a href="#">Contact Us</a></li>
         </ul>
       </div>
 
       <div>
         <h6>SITES</h6>
         <ul>
-          <li>DC</li>
-          <li>MAD Magazine</li>
-          <li>DC Kids</li>
-          <li>DC Universe</li>
-          <li>DC Power Visa</li>
+          <li><a href="#">DC</a></li>
+          <li><a href="#">MAD Magazine</a></li>
+          <li><a href="#">DC Kids</a></li>
+          <li><a href="#">DC Universe</a></li>
+          <li><a href="#">DC Power Visa</a></li>
         </ul>
       </div>
 
       <div>
         <h6>SHOP</h6>
         <ul>
-          <li>Shop DC</li>
-          <li>Shop DC Collectibles</li>
+          <li><a href="#">Shop DC</a></li>
+          <li><a href="#">Shop DC Collectibles</a></li>
         </ul>
       </div>
 
