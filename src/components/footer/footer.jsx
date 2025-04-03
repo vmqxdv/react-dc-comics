6ed44704a104ab6ffbdeffb30be3bdbbb5b49088
vmqxdv <vquintilio@gmail.com>
2025-04-03 20:02:53 +0200

    feat: Importato Info

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,12 @@
 import Navigation from "./navigation"
+import Info from "./info"
 
 export default function footer() {
   return (
     <footer>
 
       <Navigation/>
+      <Info/>
 
     </footer>
   )
