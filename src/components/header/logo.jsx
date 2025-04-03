f796410090e3fc9f37be55947a49479ec432177f
vmqxdv <vquintilio@gmail.com>
2025-04-03 20:37:06 +0200

    feat: Aggiunto div

--- src/components/header/logo.jsx
+++ src/components/header/logo.jsx
@@ -1,9 +1,9 @@
 export default function logo() {
   return (
-    <>
+    <div>
       <a href="#">
         <img src="src/assets/img/dc-logo.png" alt="DC_logo"/>
       </a>
-    </>
+    </div>
   )
 }
