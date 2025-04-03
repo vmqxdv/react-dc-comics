c8daea8e14392ad56da8bf97fe122525cc91ad86
vmqxdv <vquintilio@gmail.com>
2025-04-03 19:34:04 +0200

    feat: Dichairata base navigation

---
+++ src/components/header/navigation.jsx
@@ -0,0 +1,50 @@
+export default function navigator() {
+  return (
+    <ul>
+
+      <li>
+        <div>
+          <a href="#">CHARACTERS</a>
+          <div class="selected-page-indicator"></div>
+        </div>
+        <div>
+          <a href="#">COMICS</a>
+          <div class="selected-page-indicator"></div>
+        </div>
+        <div>
+          <a href="#">MOVIES</a>
+          <div class="selected-page-indicator"></div>
+        </div>
+        <div>
+          <a href="#">TV</a>
+          <div class="selected-page-indicator"></div>
+        </div>
+        <div>
+          <a href="#">GAMES</a>
+          <div class="selected-page-indicator"></div>
+        </div>
+        <div>
+          <a href="#">COLLECTIBLES</a>
+          <div class="selected-page-indicator"></div>
+        </div>
+        <div>
+          <a href="#">VIDEOS</a>
+          <div class="selected-page-indicator"></div>
+        </div>
+        <div>
+          <a href="#">FANS</a>
+          <div class="selected-page-indicator"></div>
+        </div>
+        <div>
+          <a href="#">NEWS</a>
+          <div class="selected-page-indicator"></div>
+        </div>
+        <div>
+          <a href="#">SHOP</a>
+          <div class="selected-page-indicator"></div>
+        </div>
+      </li>
+
+    </ul>
+  )
+};
