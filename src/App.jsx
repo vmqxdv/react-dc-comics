e4d6fcb5eb675d25444803bff3c49e7ef1a1a0ef
vmqxdv <vquintilio@gmail.com>
2025-04-03 19:52:07 +0200

    feat: Importato Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/header/header'
 import Main from './components/main/main'
+import Footer from './components/footer/footer'
 
 export default function App() {
   return (
     <>
       <Header/>
       <Main/>
+      <Footer/>
     </>
   )
 };
