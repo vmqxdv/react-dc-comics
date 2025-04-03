a548d0f31d6ce70be89a6cca159262935fcf3af9
vmqxdv <vquintilio@gmail.com>
2025-04-03 20:27:39 +0200

    fix: Corretta classe

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import Navigation from './navigation'
 
 export default function header() {
   return (
-    <header class="bg-white d-flex justify-content-between align-self-center">
+    <header class="bg-white d-flex justify-content-between align-items-center">
       <Logo/>
       <Navigation/>  
     </header>
