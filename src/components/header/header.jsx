2870a10aff7cfa1a325e79449c4525e99794ea1c
vmqxdv <vquintilio@gmail.com>
2025-04-03 19:40:01 +0200

    feat: Aggiunto Navigatiom a header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,11 @@
 import Logo from './logo';
+import Navigation from './navigation'
 
 export default function header() {
   return (
     <header>
       <Logo/>
+      <Navigation/>  
     </header>
   )
 };
