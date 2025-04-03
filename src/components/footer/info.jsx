5d0ad714f25cfbffa5b60535af060c8f6a6b1d31
vmqxdv <vquintilio@gmail.com>
2025-04-03 20:02:07 +0200

    feat: Dichiarato info

---
+++ src/components/footer/info.jsx
@@ -0,0 +1,56 @@
+export default function info() {
+  return (
+    <div>
+
+      <div>
+        <h6>DC COMICS</h6>
+        <ul>
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>TV</li>
+          <li>Games</li>
+          <li>Videos</li>
+          <li>News</li>
+        </ul>
+      </div>
+
+      <div>
+        <h6>DC</h6>
+        <ul>
+          <li>Terms Of Use</li>
+          <li>Privacy Policy (New)</li>
+          <li>Ad Choices</li>
+          <li>Advertising</li>
+          <li>Jobs</li>
+          <li>Subscriptions</li>
+          <li>Talent Workshops</li>
+          <li>CPSC Certificates</li>
+          <li>Ratings</li>
+          <li>Shop Help</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <div>
+        <h6>SITES</h6>
+        <ul>
+          <li>DC</li>
+          <li>MAD Magazine</li>
+          <li>DC Kids</li>
+          <li>DC Universe</li>
+          <li>DC Power Visa</li>
+        </ul>
+      </div>
+
+      <div>
+        <h6>SHOP</h6>
+        <ul>
+          <li>Shop DC</li>
+          <li>Shop DC Collectibles</li>
+        </ul>
+      </div>
+
+    </div>
+  )
+};
