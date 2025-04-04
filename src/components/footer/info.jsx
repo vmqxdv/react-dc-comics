a774496e1a8c2838f247bc47ed1f1b05fd024d1a
vmqxdv <vquintilio@gmail.com>
2025-04-04 18:48:48 +0200

    feat: Resa dinamica la sezione `info`

--- src/components/footer/info.jsx
+++ src/components/footer/info.jsx
@@ -1,58 +1,18 @@
-export default function info() {
+export default function info(props) {
+
+  const datas = props.datas.map((data, index) => 
+    <div>
+      <h6>{data.category}</h6>
+      <ul>
+        {data.links.map(link => <li key={`${link.text}_${index}`}><a href={link.url}>{link.text}</a></li>)}
+      </ul>
+    </div>
+  )
+
   return (
     <div className="info">
 
-      <div className="links">
-        <div>
-          <h6>DC COMICS</h6>
-          <ul>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Comics</a></li>
-          <li><a href="#">Movies</a></li>
-          <li><a href="#">TV</a></li>
-          <li><a href="#">Games</a></li>
-          <li><a href="#">Videos</a></li>
-          <li><a href="#">News</a></li>
-        </ul>
-
-        </div>
-
-        <div>
-          <h6>DC</h6>
-          <ul>
-            <li><a href="#">Terms Of Use</a></li>
-            <li><a href="#">Privacy Policy (New)</a></li>
-            <li><a href="#">Ad Choices</a></li>
-            <li><a href="#">Advertising</a></li>
-            <li><a href="#">Jobs</a></li>
-            <li><a href="#">Subscriptions</a></li>
-            <li><a href="#">Talent Workshops</a></li>
-            <li><a href="#">CPSC Certificates</a></li>
-            <li><a href="#">Ratings</a></li>
-            <li><a href="#">Shop Help</a></li>
-            <li><a href="#">Contact Us</a></li>
-          </ul>
-        </div>
-
-        <div>
-          <h6>SITES</h6>
-          <ul>
-            <li><a href="#">DC</a></li>
-            <li><a href="#">MAD Magazine</a></li>
-            <li><a href="#">DC Kids</a></li>
-            <li><a href="#">DC Universe</a></li>
-            <li><a href="#">DC Power Visa</a></li>
-          </ul>
-        </div>
-
-        <div>
-          <h6>SHOP</h6>
-          <ul>
-            <li><a href="#">Shop DC</a></li>
-            <li><a href="#">Shop DC Collectibles</a></li>
-          </ul>
-        </div>
-      </div>
+      <div className="links">{datas}</div>
 
       <div className="big-logo">
         <img src="src\assets\img\dc-logo-bg.png" alt="#" />
