a113fe9ddcdeb92f9295b81cfa9d60a1e2aab89e
vmqxdv <vquintilio@gmail.com>
2025-04-04 18:49:55 +0200

    refactor: Resa le `key` più unica

--- src/components/header/navigation.jsx
+++ src/components/header/navigation.jsx
@@ -1,6 +1,6 @@
 export default function navigation(props) {
   const pages = props.pages.map((page, index) => 
-    <li key={index}>
+    <li key={`${page.text}_${index}`}>
       <a href={page.url}>{page.text}</a>
     </li>
   )
