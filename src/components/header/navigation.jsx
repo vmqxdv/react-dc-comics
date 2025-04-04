1500a98b998c9351b8d3d26f6b6c152ac69c3491
vmqxdv <vquintilio@gmail.com>
2025-04-04 18:30:53 +0200

    feat: Resa dinamicoa la sezione `navigation`

--- src/components/header/navigation.jsx
+++ src/components/header/navigation.jsx
@@ -1,18 +1,13 @@
-export default function navigation() {
+export default function navigation(props) {
+  const pages = props.pages.map((page, index) => 
+    <li key={index}>
+      <a href={page.url}>{page.text}</a>
+    </li>
+  )
+
   return (
     <div className="navigation">
-      <ul>
-        <li><a href="#">CHARACTERS</a></li>
-        <li><a href="#">COMICS</a></li>
-        <li><a href="#">MOVIES</a></li>
-        <li><a href="#">TV</a></li>
-        <li><a href="#">GAMES</a></li>
-        <li><a href="#">COLLECTIBLES</a></li>
-        <li><a href="#">VIDEOS</a></li>
-        <li><a href="#">FANS</a></li>
-        <li><a href="#">NEWS</a></li>
-        <li><a href="#">SHOP</a></li>
-      </ul>
+      <ul>{pages}</ul>
     </div>
   )
 };
