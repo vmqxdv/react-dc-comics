04a28c398b03f6e2d347eb510ebf2d5fe0dbfbbf
vmqxdv <vquintilio@gmail.com>
2025-04-07 18:33:56 +0200

    feat: Creato file per sezione comics e dichiarato funzione per l'inserimento dei dati

---
+++ src/components/main/comics.jsx
@@ -0,0 +1,26 @@
+export default function comics(props) {
+  const { comics: comicsData } = props;
+
+  const comics = comicsData.map(comic => 
+    <li key={generateKey(comic.title)}>
+      <div className="comic-cover-container">
+        <img src={comic.thumb} alt={comic.description} />
+      </div>
+      <div className="comic-title">{comic.title}</div>
+    </li>
+  )
+
+  return (
+    <div className="comics">
+      <ul>{comics}</ul>
+    </div>
+  )
+};
+
+
+
+function generateKey(str, index) {
+  return str.toLowerCase()
+  .replaceAll(/[\W]+/g, '-')
+  .replaceAll(/^-+|-+$/g, '') + `-${index}`;
+};
